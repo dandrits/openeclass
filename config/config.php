3e5fd03df21022233d73bf2130bbc6c4c51893c8
root <root@user>
2014-10-18 15:13:10 +0300

    added facebook api integration

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-08 15:31
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'openeclassuser';
+$mysqlPassword = 'aee@s%wq';
+$mysqlMainDb = 'openeclass';
